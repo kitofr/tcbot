08278b4f8a0a17529aa5b1f7774070473570afa6
Kristoffer Roupé <kitofr@gmail.com>
2013-11-18 22:06:50 +0100

    Json

--- lib/tcbot/tcstatus.ex
+++ lib/tcbot/tcstatus.ex
@@ -1,10 +1,10 @@
 defmodule Tcbot.Tcstatus do
   
-  #@user_agent [ "User-agent", "Elixir kitofr@gmail.com" ]
+  @base_url "http://teamcity.jetbrains.com"
   alias HTTPotion.Response, as: Response
 
-  def fetch(project) do
-    case HTTPotion.get(tc_url(project)) do
+  def get(url) do
+    case HTTPotion.get(url, [{ :Accept, "application/json"}]) do
       Response[body: body, status_code: status, headers: _headers ]
       when status in 200..299 ->
         { :ok, body }
@@ -13,10 +13,11 @@ defmodule Tcbot.Tcstatus do
     end
   end
 
-  def tc_url(project) do
-    "http://teamcity.jetbrains.com/guestAuth/app/rest/projects" #builds/buildType:#{project}/statusIcon"
+  def projects do
+    "#{@base_url}/guestAuth/app/rest/projects" #builds/buildType:#{project}/statusIcon"
     #"http://teamcity.jetbrains.com/project.html?projectId=#{project}&tab=projectOverview"
     #"http://teamcity.jetbrains.com/project.html?projectId=AmazonApiClient&tab=projectOverview"
   end
+
 end
 
