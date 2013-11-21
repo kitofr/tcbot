a8d437d21bc7136f14c8da9e42cdba20dc0958de
Kristoffer Roupé <kitofr@gmail.com>
2013-11-21 21:35:51 +0100

    Failing convert_to_hashdicts test

--- test/tcbot_test.exs
+++ test/tcbot_test.exs
@@ -1,7 +1,15 @@
 defmodule TcbotTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert(true)
+  import Tcbot.Tcstatus, only: [ convert_to_hashdicts: 1 ]
+
+  test "convert" do
+    json = [{"project", 
+            [[{"id", "_Root"}, {"name", "<Root project>"}, {"href", "/guestAuth/app/rest/projects/id:_Root"}],
+            [{"id", "AmazonApiClient"}, {"name", "Amazon API client"}, {"href", "/guestAuth/app/rest/projects/id:AmazonApiClient"}]]}]
+    exct = [{:project, 
+            [[{:id, "_Root"}, {:name, "<Root project>"}, {:href, "/guestAuth/app/rest/projects/id:_Root"}],
+            [{:id, "AmazonApiClient"}, {:name, "Amazon API client"}, {:href, "/guestAuth/app/rest/projects/id:AmazonApiClient"}]]}]
+    assert convert_to_hashdicts(json) == exct
   end
 end
